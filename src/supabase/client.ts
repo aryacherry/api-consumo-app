0f780116d5a5a53e9408dbfb2999693ac52022e4
Eduardo de Andrade do Bomfim Júnior <eduardo.a.junior.dev@gmail.com>
2025-05-20 18:02:06 -0300

    :recycle: refactor: Aplica tipagem explicita às variáveis do client.ts

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { createClient, type SupabaseClient } from '@supabase/supabase-js';
 
-const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey: string = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
+const supabaseUrl: string | undefined = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey: string | undefined = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
 if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_KEY) {
   throw new Error("Variáveis de ambiente do Supabase não estão definidas.");
 }
 
-export const supabase: SupabaseClient<any, "public", any> = createClient(supabaseUrl, supabaseKey);
+export const supabase: SupabaseClient = createClient(supabaseUrl as string, supabaseKey as string);
