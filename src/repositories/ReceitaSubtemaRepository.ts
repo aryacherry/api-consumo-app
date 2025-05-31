725fecddad4994c4077ffe1158f1f1adb3e0b7cc
Eduardo de Andrade do Bomfim Júnior <eduardo.a.junior.dev@gmail.com>
2025-05-31 15:11:30 -0300

    :sparkles: feat: cria a interface ReceitaSubtema

---
+++ src/repositories/ReceitaSubtemaRepository.ts
@@ -0,0 +1,9 @@
+import { Prisma, receitas_subtemas } from "../../generated/prisma";
+
+export interface ReceitaSubtemaRepository{
+    delete(receita_id: string, subtema_id: string): Promise<void>;
+    create(receita_subtema: Prisma.receitas_subtemasUncheckedCreateInput): Promise<receitas_subtemas>;
+    findAll(): Promise<receitas_subtemas[]>;
+    findById(receita_id: string, subtema_id: string): Promise<receitas_subtemas | null>;
+    update(receita_id: string, subtema_id: string, receita_subtema: Prisma.receitas_subtemasUncheckedCreateInput): Promise<receitas_subtemas | null>;
+}
