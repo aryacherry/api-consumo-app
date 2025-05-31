f8d2a7b68eab6d1aceab606d7ac7de1132c5f635
Eduardo de Andrade do Bomfim Júnior <eduardo.a.junior.dev@gmail.com>
2025-05-31 15:12:17 -0300

    :sparkles: feat: cria a classe PrismaReceitaSubtemaRepository

---
+++ src/repositories/prisma/PrismaReceitaSubtemaRepository.ts
@@ -0,0 +1,56 @@
+
+import { Prisma, PrismaClient, receitas_subtemas } from "../../../generated/prisma";
+import { ReceitaSubtemaRepository } from "../ReceitaSubtemaRepository";
+
+export class PrismaReceitaSubtemaRepository implements ReceitaSubtemaRepository{
+    private prisma: PrismaClient;
+
+    constructor(){
+        this.prisma = new PrismaClient();
+    }
+    
+    async delete(receita_id: string, subtema_id: string): Promise<void> {
+        await this.prisma.receitas_subtemas.delete({
+            where: { 
+                receita_id_subtema_id: {
+                    receita_id,
+                    subtema_id
+                }
+            },
+        });
+    }
+    
+    async create(receita_subtema: Prisma.receitas_subtemasUncheckedCreateInput): Promise<receitas_subtemas> {
+        return await this.prisma.receitas_subtemas.create({
+            data: receita_subtema,
+        });
+    }
+
+    async findAll(): Promise<receitas_subtemas[]> {
+        return await this.prisma.receitas_subtemas.findMany();
+    }
+
+    async findById(receita_id: string, subtema_id: string): Promise<receitas_subtemas | null> {
+        return await this.prisma.receitas_subtemas.findUnique({
+            where: { 
+                receita_id_subtema_id: {
+                    receita_id,
+                    subtema_id
+                }
+             },
+        });
+    }
+
+    async update(receita_id: string, subtema_id: string, receita_subtema: Prisma.receitas_subtemasUncheckedCreateInput): Promise<receitas_subtemas | null> {
+        return await this.prisma.receitas_subtemas.update({
+            where: { 
+                receita_id_subtema_id: {
+                    receita_id,
+                    subtema_id
+                }
+            },
+            data: receita_subtema
+        })
+    }
+
+}
