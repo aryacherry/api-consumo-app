ce10023650bd356ddac5473484112fe40769dbfd
Eduardo de Andrade do Bomfim Júnior <eduardo.a.junior.dev@gmail.com>
2025-06-01 04:44:18 -0300

    :recycle: refactor: substitui temas_subtemas por acesso direto à tabela subtemas

--- src/repositories/prisma/PrismaTemaSubtemaRepository.ts
+++ src/repositories/prisma/PrismaTemaSubtemaRepository.ts
@@ -1,32 +1,34 @@
-import { Prisma, temas_subtemas } from "../../../generated/prisma";
+import { Prisma, subtemas } from "../../../generated/prisma";
 import { TemaSubtemaRepository } from "../TemaSubtemaRepository";
 import { PrismaClient } from "../../../generated/prisma/client";
 
 export class PrismaTemaSubtemaRepository implements TemaSubtemaRepository {
     private prisma: PrismaClient;
+    
     constructor() {
         this.prisma = new PrismaClient();
     }
-    async create({ tema_id, subtema_id }: Prisma.temas_subtemasUncheckedCreateInput) {
-        return this.prisma.temas_subtemas.create({
+    
+    async create({ tema_id, nome, descricao }: Prisma.subtemasUncheckedCreateInput) {
+        return this.prisma.subtemas.create({
             data: {
                 tema_id,
-                subtema_id
+                nome,
+                descricao,
             }
         });
     }
 
-    async findByTemaAndSubtema({ temaId, subtemaId }: { temaId?: NonNullable<temas_subtemas['tema_id']>; subtemaId?: NonNullable<temas_subtemas['subtema_id']> }) {
-        return this.prisma.temas_subtemas.findMany({
+    async findByTemaAndSubtema({ temaId }: { temaId?: NonNullable<subtemas['tema_id']>; subtemaId?: NonNullable<subtemas['id']> }) {
+        return this.prisma.subtemas.findMany({
             where: {
-                tema_id: temaId,
-                subtema_id: subtemaId
+                tema_id: temaId
             }
         });
     }
 
-    async getSubtemasByTema({ temaId }: { temaId: NonNullable<temas_subtemas['tema_id']> }) {
-        return this.prisma.temas_subtemas.findMany({
+    async getSubtemasByTema({ temaId }: { temaId: NonNullable<subtemas['tema_id']> }) {
+        return this.prisma.subtemas.findMany({
             where: { tema_id: temaId }
         });
     }
