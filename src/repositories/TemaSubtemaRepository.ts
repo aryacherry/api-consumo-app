d735bd13985dbb735c7cb7994f7014c8b1573890
Eduardo de Andrade do Bomfim Júnior <eduardo.a.junior.dev@gmail.com>
2025-06-01 04:35:19 -0300

    :recycle: refactor: substituir temas_subtemas por subtemas no TemaSubtemaRepository

--- src/repositories/TemaSubtemaRepository.ts
+++ src/repositories/TemaSubtemaRepository.ts
@@ -1,7 +1,7 @@
-import { Prisma, temas_subtemas } from "../../generated/prisma";
+import { Prisma, subtemas } from "../../generated/prisma";
 
 export interface TemaSubtemaRepository {
-    create({tema_id, subtema_id}: Prisma.temas_subtemasUncheckedCreateInput): Promise<temas_subtemas>;
-    findByTemaAndSubtema({ temaId, subtemaId }: { temaId?: NonNullable<temas_subtemas['tema_id']>; subtemaId?: NonNullable<temas_subtemas['subtema_id']> }): Promise<temas_subtemas[]>;
-    getSubtemasByTema({ temaId }: { temaId: NonNullable<temas_subtemas['tema_id']> }): Promise<temas_subtemas[]>;
+    create(subtema: Prisma.subtemasUncheckedCreateInput): Promise<subtemas>;
+    findByTemaAndSubtema({ temaId }: { temaId?: NonNullable<subtemas['tema_id']>; subtemaId?: NonNullable<subtemas['id']> }): Promise<subtemas[]>;
+    getSubtemasByTema({ temaId }: { temaId: NonNullable<subtemas['tema_id']> }): Promise<subtemas[]>;
 }
