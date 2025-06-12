43cf50a9cc0d21893dff9799cda4ea80fb2e57b4
Erick Barros Ferreira Gomes <erickgomes.developer@gmail.com>
2025-06-12 00:24:23 -0300

    refactor: padroniza a sintaxe de importação e remove ponto e vírgula em TemaRepository e PrismaSubtemaRepository

--- src/repositories/TemaRepository.ts
+++ src/repositories/TemaRepository.ts
@@ -1,10 +1,10 @@
-import { Prisma, subtemas, temas } from "../../generated/prisma";
+import { type Prisma, subtemas, type temas } from '../../generated/prisma'
 
 export interface TemaRepository {
-    findAll(): Promise<temas[]>;
-    findById({ id }: Pick<temas, 'id'>): Promise<temas | null>;
-    findByName({ nome }: Pick<temas, 'nome'>): Promise<temas | null>;
-    delete({ id }: Pick<temas, 'id'>): Promise<void>;
-    create(data: Prisma.temasUncheckedCreateInput): Promise<temas>;
+    findAll(): Promise<temas[]>
+    findById({ id }: Pick<temas, 'id'>): Promise<temas | null>
+    findByName({ nome }: Pick<temas, 'nome'>): Promise<temas | null>
+    delete({ id }: Pick<temas, 'id'>): Promise<void>
+    create(data: Prisma.temasUncheckedCreateInput): Promise<temas>
     /* checkIfExists(id: number): Promise<boolean>; */
 }
