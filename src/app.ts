ec9f25e3fa812597b7ea06e5c41cc0dd696ddcce
Erick Barros Ferreira Gomes <erickgomes.developer@gmail.com>
2025-06-15 22:10:25 -0300

    refactor(dicaController, PrismaDicaRepository): corrige ID da dica ao atualizar e simplifica método de atualização

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-import swaggerJsDoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
-import userRoutes from './routes/userRoutes';
-import dicasRoutes from './routes/dicaRoutes';
-import temaRoutes from './routes/temaRoutes';
-import receitaRoutes from './routes/receitaRoutes';
-import ingredientesRoutes from './routes/ingredienteRoutes';
-import type { ErrorRequestHandler } from 'express';
-import { ZodError } from 'zod';
-import quizRoutes from './routes/quizRoutes';
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import swaggerJsDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+import userRoutes from './routes/userRoutes'
+import dicasRoutes from './routes/dicaRoutes'
+import temaRoutes from './routes/temaRoutes'
+import receitaRoutes from './routes/receitaRoutes'
+import ingredientesRoutes from './routes/ingredienteRoutes'
+import type { ErrorRequestHandler } from 'express'
+import { ZodError } from 'zod'
+import quizRoutes from './routes/quizRoutes'
 
 const swaggerOptions = {
     definition: {
@@ -18,7 +18,8 @@ const swaggerOptions = {
         info: {
             title: 'API APP',
             version: '1.0.0',
-            description: 'Documentação da API usada para o desenvolvimento dos aplicativos ecológicos',
+            description:
+                'Documentação da API usada para o desenvolvimento dos aplicativos ecológicos',
         },
         servers: [
             {
@@ -41,18 +42,19 @@ const swaggerOptions = {
         },
     },
     apis: ['./src/routes/*.ts'],
-};
+}
 
-const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
+const CSS_URL =
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css'
 
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(helmet());
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-app.use(express.json({ limit: '50mb' }));
+app.use(cors())
+app.use(helmet())
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
+app.use(express.json({ limit: '50mb' }))
 app.use(
     '/api-docs',
     swaggerUi.serve,
@@ -68,25 +70,26 @@ app.use(
                     }
                 `,
         customCssUrl: CSS_URL,
-    })
-);
+    }),
+)
 
-
-app.use('/api', userRoutes);
-app.use('/api', dicasRoutes);
-app.use('/api', temaRoutes);
-app.use('/api', receitaRoutes);
-app.use('/api', ingredientesRoutes);
-app.use('/api', quizRoutes);
+app.use('/api', userRoutes)
+app.use('/api', dicasRoutes)
+app.use('/api', temaRoutes)
+app.use('/api', receitaRoutes)
+app.use('/api', ingredientesRoutes)
+app.use('/api', quizRoutes)
 
 app.use(<ErrorRequestHandler>((err, _req, res, _next) => {
-    console.error(err)
+    if (process.env.NODE_ENV !== 'production') {
+        console.error(err)
+    }
     if (err instanceof ZodError) {
         res.status(400).json({ error: err.issues })
         return
     }
-    if (err instanceof Error) {
-        res.status(400).json({ error: err.message })
+    if (err instanceof SyntaxError) {
+        res.status(400).json({ error: 'Erro de sintaxe, json malformado' })
         return
     }
     res.status(500).json({ error: 'Erro interno no servidor' })
